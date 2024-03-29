70c50d40fff97d37020f4054ed0b86e0d3a32bc9
Danyal Moazzam <danyalmoazzamjan@gmail.com>
2024-03-29 08:42:06 +0500

    Number Guessing

---
+++ node_modules/number-guessing-game-cli-madebydanyal/index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tWelcome to the Number Guessing Game! \n\t-------------------------------- \n\tMade by Danyal \n");
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt({
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Guess a number between 1 and 10",
+});
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("You guessed it!");
+}
+else {
+    console.log("You are wrong. The number was " + randomNumber);
+}
